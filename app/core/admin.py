71e91e6ff7034e60dca94b83b4c0daad6b97246e
memetics19 <shreeda.bhat@yahoo.com>
2020-06-28 13:49:09 -0700

    list user

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from core import models 
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ["id"]
+    list_display = ["email","name"]
+
+
+admin.site.register(models.User,UserAdmin)
