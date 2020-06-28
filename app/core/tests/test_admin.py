8cc01df590d933d83533060859d2fbd40312fe08
memetics19 <shreeda.bhat@yahoo.com>
2020-06-28 14:08:11 -0700

    modified admin page

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -26,3 +26,10 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """Test that the user edit page works"""
+        url = reverse('admin:core_user_change',args = [self.user.id])
+        # /admin/core/user/
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
