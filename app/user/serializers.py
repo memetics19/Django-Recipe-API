516a1a6004da64732ba4505f377c1e96af26b716
memetics19 <shreeda.bhat@yahoo.com>
2020-06-29 04:28:35 -0700

    updated user views and serializers

---
+++ app/user/serializers.py
@@ -0,0 +1,21 @@
+from django.contrib.auth import get_user_model 
+from rest_framework import serializers 
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer for the users object"""
+
+    class Meta:
+        model = get_user_model()
+        fields = ('email','password','name')
+        extra_kwargs ={'password':
+        {'write_only':True, 
+        'min_length':8
+        }
+        }
+
+    def create(self, validated_data):
+        """Create a new user with encrypted password and return"""
+        return get_user_model().objects.create_user(**validated_data)
+        
+
---
+++ app/user/tests/test_user_api.py
@@ -0,0 +1,54 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from rest_framework.test import APIClient
+from rest_framework import status
+
+CREATE_USER_URL = reverse('user:create')
+
+
+def create_user(**params):
+    return get_user_model().objects.create_user(**params)
+
+class PublicUserApiTest(TestCase):
+    """Test the user's API(Public)"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_valid_user_success(self):
+        """Test creating user is successful"""
+        payload = {
+            'email':'test@namaskar.in',
+            'password':'testpass',
+            'name':'test'
+        }
+        res = self.client.post(CREATE_USER_URL,payload)
+
+        self.assertEqual(res.status_code,status.HTTP_201_CREATED)
+        user = get_user_model().objects.get(**res.data)
+        self.assertTrue(user.check_password(payload['password']))
+        self.assertNotIn('password',res.data)
+
+    def test_user_exists(self):
+        """test created user already exists """
+
+        payload = {'email':'test@namaskar.in','password':'test123'}
+        create_user(**payload)
+
+        res = self.client.post(CREATE_USER_URL,payload)
+
+        self.assertEqual(res.status_code,status.HTTP_400_BAD_REQUEST)
+
+    def test_password_too_short(self):
+        """Test the password should be more than 8  password"""
+        payload = {'email':'test@namaskar.in','password':'test123','name':'test'}
+        res = self.client.post(CREATE_USER_URL,payload)
+
+
+        self.assertEqual(res.status_code,status.HTTP_400_BAD_REQUEST)
+        user_exists = get_user_model().objects.filter(
+            email = payload['email']
+        ).exists()
+        self.assertFalse(user_exists)
+
