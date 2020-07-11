04498a24510a61957ff3dd834a2650a4e86f0fd2
memetics19 <shreeda.bhat@yahoo.com>
2020-07-11 02:55:29 -0700

    updated manage profile

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -18,7 +18,17 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """Create a new user with encrypted password and return"""
         return get_user_model().objects.create_user(**validated_data)
-        
+
+    def update(self, instance, validated_data):
+        """Update a user, setting the password correctly and rerun it"""
+        password = validated_data.pop('password',None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+        return user       
+
 class AuthTokenSerializer(serializers.Serializer):
     """Serializer for the user authentication object """
 
