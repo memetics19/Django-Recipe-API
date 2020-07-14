902af60ffeb027660718953222a0619c19ec9071
memetics19 <shreeda.bhat@yahoo.com>
2020-07-14 04:02:00 -0700

    Added Tests for ingredients model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -57,3 +57,12 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(tag),tag.name)
+
+    def test_ingredient_str(self):
+        """Test the ingredient string representation"""
+        ingredient = models.Ingredient.objects.create(
+            user = sample_user(),
+            name = 'super'
+        )
+
+        self.assertEqual(str(ingredient),ingredient.name)
