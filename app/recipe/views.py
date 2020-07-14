945ee3f7bdcfe4575c751aa602753d647477e17f
memetics19 <shreeda.bhat@yahoo.com>
2020-07-14 03:58:43 -0700

    updated views

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication 
 from  rest_framework.permissions import IsAuthenticated 
 
-from core.models import Tag
+from core.models import Tag, Ingredient
 from recipe import serializers
 
 class TagViewSet(viewsets.GenericViewSet, 
@@ -16,9 +16,22 @@ class TagViewSet(viewsets.GenericViewSet,
 
     def get_queryset(self):
         """Return objects for the current authenticated user"""
-        return self.queryset.filter(user = self.request.user).order_by('-name')
+        return self.queryset.filter(user = self.request.user).order_by('name')
 
     def perform_create(self, serializer):
         """Create a new tag"""
         serializer.save(user=self.request.user)
+    
+
+class IngredientViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    """Manage tags in the db"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Ingredient.objects.all()
+    serializer_class = serializers.IngredientSerializer
+
+    def get_queryset(self):
+        """Return objects for the current authenticated user"""
+        return self.queryset.filter(user = self.request.user).order_by('name')
+
     
