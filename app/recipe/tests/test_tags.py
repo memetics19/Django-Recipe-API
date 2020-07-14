57e949edc1d1b00cfe30ab4e523c0b08bdeff7a4
memetics19 <shreeda.bhat@yahoo.com>
2020-07-13 20:29:39 -0700

    updated recipe tags

---
+++ app/recipe/tests/test_tags.py
@@ -0,0 +1,63 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import TestCase
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import Tag
+
+from recipe.serializers import TagSerializer
+
+TAGS_URL = reverse("recipe:tag-list")
+
+class PublicTagsApiTests(TestCase):
+    """ Test the publicly available tags API"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_login_required(self):
+        """Test that login is required for tags"""
+        res = self.client.get(TAGS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+class PrivateTagsApiTests(TestCase):
+    """Test the authorized user Tags api"""
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            'test@namaskar.in',
+            'password@1'
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_tags(self):
+        """Test retrieving the tags"""
+
+        Tag.objects.create(user=self.user, name = "Egg")
+        Tag.objects.create(user=self.user, name = "desi food")
+
+        res = self.client.get(TAGS_URL)
+
+        tags = Tag.objects.all().order_by('-name')
+        serializer = TagSerializer(tags,many=True)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_tags_limited_to_user(self):
+        """Test the tags returned for the authenticated user"""
+        user2 = get_user_model().objects.create_user(
+            'hello@namaskar.in',
+            'testpass'
+        )
+        Tag.objects.create(user=user2, name ="test")
+        tag = Tag.objects.create(user = self.user, name= "test-food")
+
+        res = self.client.get(TAGS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data[0]['name'],tag.name)
