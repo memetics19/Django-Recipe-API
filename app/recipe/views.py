80d285c797603d23f19e00a14716f452fac3e014
memetics19 <shreeda.bhat@yahoo.com>
2020-07-13 21:11:24 -0700

    updated views for creating tags

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -5,7 +5,9 @@ from  rest_framework.permissions import IsAuthenticated
 from core.models import Tag
 from recipe import serializers
 
-class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class TagViewSet(viewsets.GenericViewSet, 
+                 mixins.ListModelMixin,
+                 mixins.CreateModelMixin):
     """Manage tags in the db"""
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
@@ -15,4 +17,8 @@ class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     def get_queryset(self):
         """Return objects for the current authenticated user"""
         return self.queryset.filter(user = self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """Create a new tag"""
+        serializer.save(user=self.request.user)
     
