7dbcd368f3bc4941d7e7ca8c5d7101ee5f6a1197
Shreeda Bhat <shreeda.bhat@yahoo.com>
2021-06-11 19:23:00 +0530

    added support for detached

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -8,5 +8,5 @@ array=(${string//,/ })
 for i in "${!array[@]}"; do
   echo "Deploying information to EC2 and Gitlab"
   echo "Deploy project on server ${array[i]}"
-  ssh ubuntu@${array[i]}  "cd django-recipe-api && sudo docker build  -t docsify/demo . &&  sudo docker run -itp 3000:3000 --name=docsify -v $(pwd):/docs docsify/demo   "
+  ssh ubuntu@${array[i]}  "cd django-recipe-api && sudo docker build  -t docsify/demo . &&  sudo docker run -d -itp 3000:3000 --name=docsify -v $(pwd):/docs docsify/demo   "
 done 
