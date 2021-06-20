dc7b232df978fd9a2da1ff8d5d54fe02f702ae0e
Shreeda Bhat <shreeda.bhat@codemancers.com>
2021-06-20 21:09:23 +0530

    kubernetes DB

--- app/app/settings.py
+++ app/app/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'kubernetes_django',
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT', 5432)
+     }
 }
 
 
